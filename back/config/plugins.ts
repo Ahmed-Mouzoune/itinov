fca7481d445fec57635bbc1dca27a6df286215fb
unknown <ahmed.mouzoune92@gmail.com>
2024-06-05 05:14:45 +0200

    refactor: delete useless code back/config/plugins.ts

--- back/config/plugins.ts
+++ back/config/plugins.ts
@@ -1,14 +1,1 @@
-// path: ./config/plugins.ts
 
-const crypto = require('crypto');
-
-export default ({ env }) => ({
-  // ...
-  'users-permissions': {
-    config: {
-      jwt: {
-        jwtSecret: env('JWT_SECRET') || crypto.randomBytes(16).toString('base64')
-      },
-    },
-  },
-});
